11bc04ac8c5bd402b67b772e9297acb53182391f
fjy128 <fjy_128@163.com>
2018-11-13 17:58:07 +0800

    添加服务器缓存文件

--- src/app.js
+++ src/app.js
@@ -1,14 +1,12 @@
 const http = require('http')
-
-const hostname = '127.0.0.1'
-const port = 3000
+const conf = require('./config/defaultConfig')
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello  World\n')
+  res.end('Hello World\n')
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+server.listen(conf.port, conf.hostname, () => {
+  console.log(`Server running at http://${conf.hostname}:${conf.port}/`)
 })
