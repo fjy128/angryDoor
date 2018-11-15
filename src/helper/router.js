46d1fc3b7ad957413fbf88699e89255552d62536
fjy128 <fjy_128@163.com>
2018-11-15 18:09:29 +0800

    添加promisify

--- src/helper/router.js
+++ src/helper/router.js
@@ -2,24 +2,33 @@ const fs = require('fs')
 const promisify = require('util').promisify
 const stat = promisify(fs.stat)
 const readdir = promisify(fs.readdir)
-module.expires = async function (req, res, filePath) {
+module.exports = async function (req, res, filePath) {
   try {
-    const stats = stat(filePath)
-    if (stats.isFile()) {
+    const stats = await stat(filePath)
+    let isFile = stats.isFile()
+    let isDirectory = stats.isDirectory()
+    if (isFile) {
       res.statusCode = 200
       res.setHeader('Content-Type', 'text/plain')
       fs.createReadStream(filePath).pipe(res)
-    } else if (stats.isDirectory()) {
+    } else if (isDirectory) {
       const files = await readdir(filePath)
-      try {
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/plain')
-        res.end(files.join(','))
-      } catch (err) {
-        res.statusCode = 404
-        res.setHeader('Content-Type', 'text/plain')
-        res.end(`${filePath}not found`)
-      }
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/plain')
+      res.end(files.join(','))
+      // try {
+      //   res.statusCode = 200
+      //   res.setHeader('Content-Type', 'text/plain')
+      //   res.end(files.join(','))
+      // } catch (err) {
+      //   res.statusCode = 404
+      //   res.setHeader('Content-Type', 'text/plain')
+      //   res.end(`${filePath}not found`)
+      // }
+    } else {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'text/plain')
+      res.end('ok')
     }
   } catch (ex) {
     console.error(ex)
